c6146767e3e3c4a3f94c5773639ab453edccc289
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-02-02 19:20:55 +0300

    Нужно больше функций доработка

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@
 function getRandomFloat(from, to, decimal = 2) {
   if (to > from && from >= 0) {
     const result = (Math.random() * to).toFixed(decimal);
-    return result >= from ? result : getRandomFloat(from, to, decimal);
+    if (result >= from) {
+      return result;
+    }
   }
-
   throw new Error('Input data error');
 }
 
 getRandomFloat();
+
+function getRandomInt(from, to) {
+  if (to > from && from >= 0) {
+    from = Math.ceil(from);
+    to = Math.floor(to);
+    return Math.floor(Math.random() * (to - from)) + from;
+  }
+  throw new Error('Input data error');
+}
+
+getRandomInt();
