405176599eca85a51ce2b129cd2b1d82e9444871
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-02-08 15:45:56 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 
 function getRandomFloat(from, to, decimal = 2) {
   if (to > from && from >= 0) {
-    const result = (Math.random() * to).toFixed(decimal);
-    if (result >= from) {
-      return result;
-    }
+    return (Math.random() * to).toFixed(decimal);
   }
   throw new Error('Input data error');
 }
-
-getRandomFloat();
 
 function getRandomInt(from, to) {
   if (to > from && from >= 0) {
@@ -20,4 +15,46 @@ function getRandomInt(from, to) {
   throw new Error('Input data error');
 }
 
-getRandomInt();
+const getRandomArrayElement = function (elements) {
+  return elements[getRandomInt(0, elements.length - 1)];
+};
+
+const dwellings = ['palace', 'flat', 'house', 'bungalow'];
+const time = ['12:00', '13:00', '14:00'];
+const extras = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const appearances = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+const locationX = getRandomFloat(35.65000, 35.70000);
+const locationY = getRandomFloat(139.70000, 139.80000);
+const avatarId = getRandomInt(1, 8);
+
+const offer_count = 1;
+
+const createOffer = function () {
+  return {
+    author: {
+      avatar: `img/avatars/user0${avatarId}.png`,
+    },
+    offer: {
+      title: 'Та самая квартира',
+      address: locationX + ' ' + locationY,
+      price: 45,
+      type: getRandomArrayElement(dwellings),
+      rooms: 2,
+      guests: 3,
+      checkin: getRandomArrayElement(time),
+      checkout: getRandomArrayElement(time),
+      features: getRandomArrayElement(extras),
+      description: 'Окна выходят во двор, на запад. Прямые лучи солнца попадают в окна только вечером.',
+      photos: getRandomArrayElement(appearances),
+    },
+    location: {
+      x: locationX,
+      y: locationY,
+    },
+  }
+}
+
+const randomElements = new Array(offer_count).fill(null).map(() => createOffer());
+
+randomElements();
