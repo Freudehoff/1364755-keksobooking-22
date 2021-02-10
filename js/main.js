36d494de86ea8672b3c14b4717fcd37520bc15eb
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-02-10 16:16:45 +0300

    доработка

--- js/main.js
+++ js/main.js
@@ -19,42 +19,53 @@ const getRandomArrayElement = function (elements) {
   return elements[getRandomInt(0, elements.length - 1)];
 };
 
-const dwellings = ['palace', 'flat', 'house', 'bungalow'];
-const time = ['12:00', '13:00', '14:00'];
-const extras = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const appearances = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const getRandomArrayLength = function (elements) {
+  const newArray = [];
+  let elementsLen = elements.length;
 
-const locationX = getRandomFloat(35.65000, 35.70000);
-const locationY = getRandomFloat(139.70000, 139.80000);
-const avatarId = getRandomInt(1, 8);
+  for(let i=getRandomInt(0, elementsLen); i < elementsLen; i+=getRandomInt(1, elementsLen)) {
+    newArray.push(elements[i]);
+  }
 
-const offer_count = 1;
+  return newArray;
+}
+
+const DWELLINGS = ['palace', 'flat', 'house', 'bungalow'];
+const TIME = ['12:00', '13:00', '14:00'];
+const EXTRAS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const APPEARANCES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+const LOCATION_X = getRandomFloat(35.65000, 35.70000);
+const LOCATION_Y = getRandomFloat(139.70000, 139.80000);
+const AVATAR_ID = getRandomInt(1, 8);
+
+const OFFER_COUNT = 1;
 
 const createOffer = function () {
   return {
     author: {
-      avatar: `img/avatars/user0${avatarId}.png`,
+      avatar: `img/avatars/user0${AVATAR_ID}.png`,
     },
     offer: {
       title: 'Та самая квартира',
-      address: locationX + ' ' + locationY,
+      address: LOCATION_X + ' ' + LOCATION_Y,
       price: 45,
-      type: getRandomArrayElement(dwellings),
+      type: getRandomArrayElement(DWELLINGS),
       rooms: 2,
       guests: 3,
-      checkin: getRandomArrayElement(time),
-      checkout: getRandomArrayElement(time),
-      features: getRandomArrayElement(extras),
+      checkin: getRandomArrayElement(TIME),
+      checkout: getRandomArrayElement(TIME),
+      features: getRandomArrayLength(EXTRAS),
       description: 'Окна выходят во двор, на запад. Прямые лучи солнца попадают в окна только вечером.',
-      photos: getRandomArrayElement(appearances),
+      photos: getRandomArrayLength(APPEARANCES),
     },
     location: {
-      x: locationX,
-      y: locationY,
+      x: LOCATION_X,
+      y: LOCATION_Y,
     },
   }
 }
 
-const randomElements = new Array(offer_count).fill(null).map(() => createOffer());
+const randomElements = new Array(OFFER_COUNT).fill(null).map(() => createOffer());
 
-randomElements();
+randomElements;
