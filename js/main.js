a1bbf3feff204c071ffda1742d5da36a1288441b
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-02-11 08:56:51 +0300

    доработка 3

--- js/main.js
+++ js/main.js
@@ -38,13 +38,17 @@ const APPEARANCES = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http
 const OFFER_COUNT = 10;
 
 const createOffer = function () {
+  const LOCATION_X = getRandomFloat(35.65000, 35.70000);
+  const LOCATION_Y = getRandomFloat(139.70000, 139.80000);
+  const AVATAR_ID = getRandomInt(1, 8);
+
   return {
     author: {
-      avatar: `img/avatars/user0${getRandomInt(1, 8)}.png`,
+      avatar: `img/avatars/user0${AVATAR_ID}.png`,
     },
     offer: {
       title: 'Та самая квартира',
-      address: getRandomFloat(35.65000, 35.70000) + ' ' + getRandomFloat(139.70000, 139.80000),
+      address: LOCATION_X + ' ' + LOCATION_Y,
       price: 45,
       type: getRandomArrayElement(DWELLINGS),
       rooms: 2,
@@ -56,8 +60,8 @@ const createOffer = function () {
       photos: getRandomArrayLength(APPEARANCES),
     },
     location: {
-      x: getRandomFloat(35.65000, 35.70000),
-      y: getRandomFloat(139.70000, 139.80000),
+      x: LOCATION_X,
+      y: LOCATION_Y,
     },
   }
 }
