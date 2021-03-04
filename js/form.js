63827891c063eb4bf2af1c4186d1eec36bb0ef6a
Вячеслав Жубрин <vyacheslavzhubrin@gmail.com>
2021-03-04 17:00:35 +0300

    Помощь друга (часть 2)

--- js/form.js
+++ js/form.js
@@ -5,6 +5,15 @@ const priceInput = document.querySelector('#price');
 const timeInInput = document.querySelector('#timein');
 const timeOutInput = document.querySelector('#timeout');
 const addressInput = document.querySelector('#address');
+const titleInput = document.querySelector('#title');
+const roomNumberInput = document.querySelector('#room_number');
+const guestsInput = document.querySelector('#capacity');
+
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE = 1000000;
+const MAX_ROOM = 100;
+const NOT_FOR_GUESTS = 0;
 
 typeInput.addEventListener('change', function (evt) {
   switch (evt.target.value) {
@@ -29,11 +38,62 @@ typeInput.addEventListener('change', function (evt) {
 
 timeInInput.addEventListener('change', function (evt) {
   timeOutInput.value = evt.target.value;
-})
+});
 
 timeOutInput.addEventListener('change', function (evt) {
   timeInInput.value = evt.target.value;
-})
+});
+
+const validationRoomsAndGuests = function () { // Валидация количества комнат и гостей
+  const roomValue = Number(roomNumberInput.value);
+  const guestsValue = Number(guestsInput.value);
+
+  if (roomValue < guestsValue && guestsValue !== NOT_FOR_GUESTS) {
+    roomNumberInput.setCustomValidity('Слишком мало комнат');
+  } else if (roomValue === MAX_ROOM & guestsValue !== NOT_FOR_GUESTS) {
+    roomNumberInput.setCustomValidity('Такое количество комнат не для гостей');
+  } else if (roomValue !== MAX_ROOM && guestsValue === NOT_FOR_GUESTS) {
+    roomNumberInput.setCustomValidity('Выберите другой вариант');
+  } else {
+    roomNumberInput.setCustomValidity('');
+  }
+
+  roomNumberInput.reportValidity();
+};
+
+roomNumberInput.addEventListener('change', function () {
+  validationRoomsAndGuests();
+});
+
+guestsInput.addEventListener('change', function () {
+  validationRoomsAndGuests();
+});
+
+titleInput.addEventListener('input', function () { // Валидация поля ввода заголовка объявления
+  const valueLength = titleInput.value.length;
+
+  if (valueLength < MIN_TITLE_LENGTH) {
+    titleInput.setCustomValidity('Ещё ' + (MIN_TITLE_LENGTH - valueLength) + ' симв.');
+  } else if (valueLength > MAX_TITLE_LENGTH) {
+    titleInput.setCustomValidity('Удалите лишние ' + (valueLength - MAX_TITLE_LENGTH) + ' симв.');
+  }  else {
+    titleInput.setCustomValidity('');
+  }
+
+  titleInput.reportValidity();
+});
+
+priceInput.addEventListener('input', function () { // Валидация поля ввода цены
+  const valuePrice = priceInput.value;
+
+  if (valuePrice > MAX_PRICE) {
+    priceInput.setCustomValidity(`Цена не может быть выше ${MAX_PRICE}`);
+  } else {
+    priceInput.setCustomValidity('');
+  }
+
+  priceInput.reportValidity();
+});
 
 addressInput.setAttribute('disabled', 'disabled');
 addressInput.value = MAIN_LAT.toFixed(5) + ' ' + MAIN_LNG.toFixed(5);
